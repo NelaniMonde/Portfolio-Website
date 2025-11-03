8a2c15e55a14160ab00d5cfce0cc1c60f2f36a92
NelaniMonde <73121180+NelaniMonde@users.noreply.github.com>
2025-11-03 15:08:28 +0200

    portfolio completion
    completed the portfolio side of the website

--- portfolioProfile/javaScript/portFlioBehavior.js
+++ portfolioProfile/javaScript/portFlioBehavior.js
@@ -19,8 +19,35 @@ function sendMail()
     //  "&body="+message+
     //  "%0D%0AFrom:%20"+name+"%0D%0A%0D%0AEmail:%20"+email;
 
+    
+    if(name.length <=0 || email.length <=0 || subject.length <=0 || message.length <=0)
+    {
+        if(name.length <=0)
+        {
+            alert("Name Missing Please Enter your name");
+        }
+
+        if(email.length <=0)
+        {
+            alert("Email Missing Please Enter your email");
+        }
+
+        if(subject.length <=0)
+        {
+            alert("Email Subject Missing Please Enter the email Subject");
+        }
+
+        
+        if(message.length <=0)
+        {
+            alert("Email Message has not been filled in Please Enter the email message");
+        }
+    }
+    else
+    {
      
      window.location.href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=nnelanimonde@gmail.com&su="+subject+
      "&body="+message+
      "%0D%0A%0D%0AFrom:%20"+name+"%0D%0AEmail:%20"+email;
+    }
 }
